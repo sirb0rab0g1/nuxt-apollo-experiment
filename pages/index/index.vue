acaec2f9cd198f8e56f0ae3a5a881c7597e3c27c
kent <kentoyfueconcillo@gmail.com>
2020-05-08 17:20:38 +0800

    adding login, register and validate account for more security measures

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,15 +1,55 @@
 <template>
-  <v-layout>
-    <v-btn @click="proceed">go to graph sample</v-btn>
-  </v-layout>
+    <v-layout row wrap justify-center>
+      <no-ssr>
+        <v-flex class="ma-3" xs12 sm8 md4 lg3>
+          <v-card class="elevation-12 pa-3">
+            <v-flex align="center" justify="center">
+            </v-flex>
+            <v-text-field
+              outlined
+              v-model="form.email"
+              label="Email Address"
+              prepend-icon="person"
+              class="px-2"/>
+           
+            <v-text-field
+              v-model="form.password"
+              label="Password"
+              prepend-icon="lock"
+              class="px-2"
+              type="password"/>
+
+            <v-btn block depressed @click="login" class="white--text" color="#fbb730">
+              LOGIN
+            </v-btn>
+            
+            <v-card-actions>
+              <span class="forgot-text pointer" @click="goTo('/register')">
+                Register
+              </span>
+              <v-spacer></v-spacer>
+              <span class="forgot-text pointer">
+                Forgot password
+              </span>
+            </v-card-actions>
+          </v-card>
+        </v-flex>
+      </no-ssr>
+    </v-layout>
 </template>
 <script>
   import Global from '~/plugins/mixins/global'
   export default {
     mixins: [Global],
+    data: () => ({
+      form: {}
+    }),
     methods: {
       proceed () {
         this.goTo('/graphqlsample')
+      },
+      login () {
+        console.log(this.form)
       }
     }
   }
---
+++ pages/index/validate/_id.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-flex>
+    {{ msg }}
+  </v-flex>
+</template>
+
+<script>
+  import {
+    VERIFY_ACCOUNT
+  } from '~/plugins/mixins/queries/auth'
+
+  export default {
+    data: () => ({
+      msg: 'validating account please wait'
+    }),
+    methods: {
+      sendvalidationtoken () {
+        console.log(this.$nuxt._route.params.id)
+        let token = { token: this.$nuxt._route.params.id }
+        this.$apollo.mutate({
+          mutation: VERIFY_ACCOUNT,
+          variables: token,
+          fetchPolicy: 'no-cache'
+        }).then(data => {
+          if (data.data.verify_account.success === true) {
+            this.msg = 'Account is now verified'
+          } else {
+            this.msg = data.data.verify_account.errors.nonFieldErrors[0].message
+          }
+        })
+      }
+    },
+    beforeRouteEnter (to, from, next) {
+      next(vm => {
+        vm.sendvalidationtoken()
+      })
+    }
+  }
+</script>
